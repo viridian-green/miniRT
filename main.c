339ab45547cba0b78bc59b93e504471e9c718b9f
Adele Martin <adelejulietmartin@gmail.com>
2025-01-14 13:53:32 +0100

    parsing file

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include "minirt.h"
+#include <fcntl.h>
 
-int main()
+int parse_map(char *config_file)
 {
 	int	fd;
 
-	fd = open(file_name, O_RDONLY);
+	if (fd < 0)
+		perror("Error. Cannot open file.");
+
+	fd = open(config_file, O_RDONLY);
+}
+
+
+int main(int ac, char **av)
+{
+	if (ac > 2)
+		perror("Error. Please enter the config file as argument.");
+	parse_map(av[1]);
 }
--- minirt.h
+++ minirt.h
@@ -1,13 +1,6 @@
 #ifndef MINIRT_H
 #define MINIRT_H
 
-typedef struct s_ambient
-{
-	int light_ratio;
-	int color;
-
-} t_ambient;
-
 typedef struct s_vector
 {
 	float x;
@@ -15,10 +8,31 @@ typedef struct s_vector
 	float z;
 } t_vector;
 
-//field of view: size of the viewport and how far away they are from camera
+typedef struct s_color
+{
+	double	r;
+	double	g;
+	double	b;
+} t_color;
+
+typedef struct s_point
+{
+	float x;
+	float y;
+	float z;
+} t_point;
+
+typedef struct s_ambient
+{
+	int light_ratio;
+	t_color color;
+
+} t_ambient;
+
+//FOV: Angle of what you see.
 typedef struct s_camera
 {
-	int coordinates;
+	t_point viewpoint;
 	t_vector orientation;
 	int fov;
 
@@ -26,25 +40,33 @@ typedef struct s_camera
 
 typedef struct s_light
 {
-	int amb_ratio;
-
+	t_point light_point;
+	int diameter;
+	t_color color;
 } t_light;
 
 typedef struct s_sp
 {
-	int amb_ratio;
-
+	t_point center;
+	float diameter;
+	t_color color;
 } t_sp;
 
 typedef struct s_pl
 {
-	int amb_ratio;
+	t_point plane_point;
+	t_vector normal_vector;
+	t_color color;
 
 } t_pl;
 
 typedef struct s_cy
 {
-	int amb_ratio;
+	t_point center;
+	t_vector axis;
+	float diameter;
+	float height;
+	t_color;
 
 } t_cy;
 
