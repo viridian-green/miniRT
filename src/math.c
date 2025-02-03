4125e445cbf0a00cb090fe0e8a5f0d8e843e4b07
mariaserpa <iza-serpa@hotmail.com>
2025-02-04 00:24:42 +0100

    insert file with math of vectors

---
+++ src/math.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   math.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 23:45:00 by mrabelo-          #+#    #+#             */
+/*   Updated: 2025/02/03 23:49:40 by mrabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/minirt.h"
+
+t_vector	vector_add(t_vector v1, t_vector v2)
+{
+	t_vector	result;
+
+	result.x = v1.x + v2.x;
+	result.y = v1.y + v2.y;
+	result.z = v1.z + v2.z;
+	return (result);
+}
+
+t_vector	vector_subtract(t_vector v1, t_vector v2)
+{
+	t_vector	result;
+
+	result.x = v1.x - v2.x;
+	result.y = v1.y - v2.y;
+	result.z = v1.z - v2.z;
+	return (result);
+}
+
+t_vector	vector_multiply(t_vector v, double scalar)
+{
+	t_vector	result;
+
+	result.x = v.x * scalar;
+	result.y = v.y * scalar;
+	result.z = v.z * scalar;
+	return (result);
+}
+
+double	vec_length(t_vector v)
+{
+	double		length;
+
+	length = sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+	return (length);
+}
