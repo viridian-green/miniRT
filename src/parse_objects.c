0102f49180d857fb5004d8b5ab03ddf9daac82fe
mariaserpa <iza-serpa@hotmail.com>
2025-02-04 00:23:45 +0100

    change the struct of sphere, cylinder and planes

--- src/parse_objects.c
+++ src/parse_objects.c
@@ -6,64 +6,112 @@
 /*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 18:04:37 by mrabelo-          #+#    #+#             */
-/*   Updated: 2025/01/27 18:32:12 by mrabelo-         ###   ########.fr       */
+/*   Updated: 2025/02/03 18:54:44 by mrabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-void	parse_plane(char *line, t_scene *scene)
+t_object	*new_object_node(void)
 {
-	char	**split_line;
+	t_object	*node;
 
-	split_line = ft_split(line, ' ');
-	if (validate_line_format(split_line, 4))
-		free_exit("Error. Invalid plane format.", scene);
-	set_coordinates(&split_line[0], &scene->pl.plane_point);
-	set_orientation(&split_line[1], &scene->pl.orientation);
-	set_color(&split_line[3], &scene->pl.color);
-	//printf("%d", scene->pl.color.b);
-	if (validate_orientation(&scene->pl.orientation) || \
-		validate_color(&scene->pl.color))
-		free_exit("Error. Invalid plane parameters.", scene);
+	node = (t_object *)ft_calloc(1, sizeof(t_object));
+	if (!node)
+		return (NULL);
+	node->next = NULL;
+	return (node);
 }
+
+void	add_node_to_object(t_object **head, t_object *node)
+{
+	t_object	*temp;
+
+	if (head)
+	{
+		if (!*head)
+			*head = node;
+		else
+		{
+			temp = *head;
+			while (temp->next)
+				temp = temp->next;
+			temp->next = node;
+		}
+	}
+}
+//verify necessity of creating function to move to last node
 
 void	parse_sphere(char *line, t_scene *scene)
 {
-	char	**split_line;
+	char		**split_line;
+	t_object	*node;
 
 	split_line = ft_split(line, ' ');
-	if (validate_line_format(split_line, 4))
-		free_exit("Error. Invalid sphere format.", scene);
-	set_coordinates(&split_line[0], &scene->sp.center);
+	if (!split_line || validate_line_format(split_line, 4))
+		handle_parse_error(split_line, scene, "Error. Invalid sphere format.");
+	node = new_object_node();
+	if (!node)
+		handle_parse_error(split_line, scene, "Error. Memory allocation failed.");
+	add_node_to_object(&scene->objects, node);
+	set_coordinates(&split_line[0], &node->sp.center);
 	if (validate_numeric_value(split_line[2]))
-		free_exit("Error. Invalid sphere diameter.", scene);
-	scene->sp.diameter = ft_atof(split_line[2]);
-	set_color(&split_line[3], &scene->sp.color);
-	//printf("Color: (%d, %d, %d)\n", scene->pl.color.r, scene->pl.color.g, scene->pl.color.b);
-	if (validate_diameter(scene->sp.diameter) || \
-		validate_color(&scene->sp.color))
-		free_exit("Error. Invalid sphere parameters.", scene);
+		handle_parse_error(split_line, scene, "Error. Invalid sphere diameter.");
+	node->sp.diameter = ft_atof(split_line[2]);
+	set_color(&split_line[3], &node->sp.color);
+	if (validate_diameter(node->sp.diameter) || validate_color(&node->sp.color))
+		handle_parse_error(split_line, scene, "Error. Invalid sphere parameters.");
+	free_split(split_line);
+	node->type = 1;
+}
+
+void	parse_plane(char *line, t_scene *scene)
+{
+	char		**split_line;
+	t_object	*node;
+
+	split_line = ft_split(line, ' ');
+	if (!split_line || validate_line_format(split_line, 4))
+		handle_parse_error(split_line, scene, "Error. Invalid plane format.");
+	node = new_object_node();
+	if (!node)
+		handle_parse_error(split_line, scene, "Error. Memory allocation failed.");
+	add_node_to_object(&scene->objects, node);
+	set_coordinates(&split_line[0], &node->pl.plane_point);
+	set_orientation(&split_line[1], &node->pl.orientation);
+	set_color(&split_line[3], &node->pl.color);
+	if (validate_orientation(&node->pl.orientation) ||
+		validate_color(&node->pl.color))
+		handle_parse_error(split_line, scene, "Error. Invalid plane parameters.");
+	free_split(split_line);
+	node->type = 2;
 }
 
 void	parse_cylinder(char *line, t_scene *scene)
 {
-	char	**split_line;
+	char		**split_line;
+	t_object	*node;
 
 	split_line = ft_split(line, ' ');
-	if (validate_line_format(split_line, 6))
-		free_exit("Error. Invalid cylinder format.", scene);
-	set_coordinates(&split_line[0], &scene->cy.center);
-	set_orientation(&split_line[1], &scene->cy.orientation);
+	if (!split_line || validate_line_format(split_line, 6))
+		handle_parse_error(split_line, scene, "Error. Invalid cylinder format.");
+	node = new_object_node();
+	if (!node)
+		handle_parse_error(split_line, scene, "Error. Memory allocation failed.");
+	add_node_to_object(&scene->objects, node);
+	set_coordinates(&split_line[0], &node->cy.center);
+	set_orientation(&split_line[1], &node->cy.orientation);
 	if (validate_numeric_value(split_line[3]) || \
 		validate_numeric_value(split_line[4]))
-		free_exit("Error. Invalid cylinder dimensions.", scene);
-	scene->cy.diameter = ft_atof(split_line[3]);
-	scene->cy.height = ft_atof(split_line[4]);
-	set_color(&split_line[5], &scene->cy.color);
-	if (validate_orientation(&scene->cy.orientation) || \
-		validate_diameter(scene->cy.diameter) || \
-		validate_height(scene->cy.height) || \
-		validate_color(&scene->cy.color))
-		free_exit("Error. Invalid cylinder parameters.", scene);
+		handle_parse_error(split_line, scene, "Error. Invalid cylinder dimensions.");
+	node->cy.diameter = ft_atof(split_line[3]);
+	node->cy.height = ft_atof(split_line[4]);
+	set_color(&split_line[5], &node->cy.color);
+	if (validate_orientation(&node->cy.orientation) || \
+		validate_diameter(node->cy.diameter) || \
+		validate_height(node->cy.height) || \
+		validate_color(&node->cy.color))
+		handle_parse_error(split_line, scene, "Error. Invalid cylinder parameters.");
+	free_split(split_line);
+	node->type = 3;
 }
