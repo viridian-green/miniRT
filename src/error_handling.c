1d301f90caa0abbe59ce57e57ba32b3b03f0db9d
mariaserpa <iza-serpa@hotmail.com>
2025-02-04 00:25:32 +0100

    include free to split char and handle errors function

--- src/error_handling.c
+++ src/error_handling.c
@@ -6,7 +6,7 @@
 /*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 14:12:40 by mrabelo-          #+#    #+#             */
-/*   Updated: 2025/01/27 17:42:11 by mrabelo-         ###   ########.fr       */
+/*   Updated: 2025/02/03 18:47:54 by mrabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,26 @@ int	free_exit(char *message, t_scene *scene)
 		free(scene);
 	perror(message);
 	exit (EXIT_FAILURE);
+}
+
+void	free_split(char **split)
+{
+	int	i;
+
+	if (!split)
+		return ;
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
+void	handle_parse_error(char **split_line, t_scene *scene, char *message)
+{
+	if (split_line)
+		free_split(split_line);
+	free_exit(message, scene);
 }
