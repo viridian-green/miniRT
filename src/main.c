cc754a16f892c64a3cb9acc70e9d0730e79bed6d
moldypeaches <adelejulietmartin@gmail.com>
2025-02-07 13:08:11 +0100

    ok

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ademarti <ademarti@student.42berlin.de     +#+  +:+       +#+        */
+/*   By: ademarti <adelemartin@student.42.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 14:10:52 by ademarti          #+#    #+#             */
-/*   Updated: 2025/02/05 18:14:07 by ademarti         ###   ########.fr       */
+/*   Updated: 2025/02/07 13:08:03 by ademarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +25,12 @@ void	key_board(mlx_key_data_t key, t_scene *scene)
 	//update(scene);
 }
 
-/*
-void	my_mlx_pixel_put(int x, int y, t_fractal *fractal, int color)
-{
-	int	offset;
 
-	offset = (y * fractal->image.size_line) + ((fractal->image.bpp / 8) * x);
-	*(unsigned int *)(fractal->image.pixel + offset) = color;
-}
-*/
-
-/*
 void	render_image(t_scene *scene)
 {
 	double		pixel_x;
 	double		pixel_y;
 	t_ray		ray;
-	uint8_t red = 255;
-	uint8_t green = 0;
-	uint8_t blue = 0;
-
-// Combine the components into a single uint32_t
-	uint8_t color = (red << 16) | (green << 8) | blue;
 
 	pixel_x = 0;
 	while (pixel_x < scene->img->width)
@@ -56,40 +40,10 @@ void	render_image(t_scene *scene)
 		{
 			ray = create_ray(pixel_x, pixel_y, scene->camera.origin, scene);
 			//put_color_to_pixel(pixel_x, pixel_y, scene, ray);
-			mlx_put_pixel(scene->img, pixel_x, pixel_y, color);
 			pixel_y++;
 		}
 		pixel_x++;
 	}
-}
-*/
-
-void render_image(t_scene *scene)
-{
-    double pixel_x;
-    double pixel_y;
-    t_ray ray;
-    uint32_t color;
-
-    uint8_t red = 255;
-    uint8_t green = 0;
-    uint8_t blue = 0;
-
-    color = (red << 16) | (green << 8) | blue;
-
-    pixel_x = 0;
-    while (pixel_x < scene->canvas_height)
-    {
-        pixel_y = 0;
-        while (pixel_y < scene->canvas_width)
-        {
-            ray = create_ray(pixel_x, pixel_y, scene->camera.origin, scene);
-            //put_color_to_pixel(pixel_x, pixel_y, scene, ray);
-            mlx_put_pixel(scene->img, pixel_x, pixel_y, color);
-            pixel_y++;
-        }
-        pixel_x++;
-    }
 }
 
 int	main(int ac, char **av)
--- src/ray.c
+++ src/ray.c
@@ -3,15 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ray.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ademarti <ademarti@student.42berlin.de     +#+  +:+       +#+        */
+/*   By: ademarti <adelemartin@student.42.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:37:21 by mrabelo-          #+#    #+#             */
-/*   Updated: 2025/02/05 17:33:22 by ademarti         ###   ########.fr       */
+/*   Updated: 2025/02/07 12:32:22 by ademarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+
+/*In this function the pixels are scaled both vertically and horizontally
+The variable p_center (pixel center) marks the exact spot on the window in 3D space for a given pixel (from 2D to 3D)
+*/
 t_ray	create_ray(double p_x, double p_y, t_vector origin, t_scene *scene)
 {
 	t_ray		ray;
